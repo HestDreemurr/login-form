ea48211d065dd71b26b1ee37d3db5473f23e23ef
Hest <kingmastercllr@gmail.com>
2024-07-20 15:16:33 -0300

    Alteração no arquivo script.js

--- static/script.js
+++ static/script.js
@@ -3,6 +3,10 @@ let res = document.querySelector("#res")
 function entrar() {
   let email = document.querySelector("#email").value
   let senha = document.querySelector("#senha").value
+  if (email.length == 0 || senha.length == 0) {
+    res.innerHTML = `<p style='color: red; font-weight: bolder;'>Preencha todos os campos!</p>`
+    return
+  }
   fetch("/auth", {
     method: "POST",
     headers: {
@@ -18,7 +22,7 @@ function entrar() {
       if (data["auth"]) {
         res.innerHTML = "<p>Login realizado com sucesso!</p>"
       } else {
-        res.innerHTML = "<p>E-mail ou senha incorretos! Tente novamente. </p>"
+        res.innerHTML = "<p style='color: red; font-weight: bolder;'>E-mail ou senha incorretos! Tente novamente.</p>"
       }
     })
 }
